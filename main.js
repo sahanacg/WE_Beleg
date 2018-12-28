f2c36b91ed3bc4a701029b13ae4c656f034d1a86
baumkuchen42 <uta@terminal.run>
2018-12-28 13:49:05 +0100

    fully working application, feature-complete so far

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var mode_chooser, home_card, start_btn, outer, home_btn;
+var mode_chooser, home_card, start_btn, outer, home_btn, import_btn, export_btn;
 
 document.addEventListener('DOMContentLoaded', function(){
     initialize();
@@ -10,15 +10,14 @@ var initialize = function(){
   mode_chooser = document.querySelector('#mode_chooser');
   start_btn = document.querySelector('#start_btn');
   home_btn = document.querySelector('#home_btn');
+  import_btn = document.querySelector('#import_btn');
+  export_btn = document.querySelector('#export_btn');
   back_to_home();
 
-  start_btn.onclick = function(){
-    start();
-  }
-
-  home_btn.onclick = function () {
-    back_to_home();
-  }
+  start_btn.onclick = start;
+  home_btn.onclick = back_to_home;
+  export_btn.onclick = export_localstorage;
+  import_btn.onclick = import_as_localstorage;
 }
 
 var start = function () {
@@ -56,6 +55,38 @@ var start_add = function () {
 var back_to_home = function () {
   //console.log('0 = ', outer.childNodes[0], '1 = ', outer.childNodes[1], '2 = ', outer.childNodes[2]);
   outer.replaceChild(home_card, outer.childNodes[2]);
+}
+
+var export_localstorage = function () {
+  var localstorage_json = JSON.stringify(localStorage);
+  var download_link = document.createElement('a');
+  download_link.setAttribute('href', 'data:application/json;charset=utf-8,'
+                              + encodeURIComponent(localstorage_json));
+  download_link.setAttribute('download', 'vocabulary.json');
+  download_link.setAttribute('style', 'display:none');
+  download_link.click();
+}
+
+var import_as_localstorage = function () {
+  var file_chooser = document.createElement('input');
+  file_chooser.setAttribute('type', 'file');
+  file_chooser.setAttribute('style', 'display:none');
+  file_chooser.setAttribute('accept', '.json');
+
+  file_chooser.onchange = function (event) {
+    var input = event.target;
+    var file_reader = new FileReader();
+
+    file_reader.onload = function () {
+      var localstorage_json = file_reader.result;
+      localstorage_json = JSON.parse(localstorage_json);
+      for (var key in localstorage_json) {
+        localStorage.setItem(key, localstorage_json[key]);
+      }
+    }
+    file_reader.readAsText(input.files[0]);
+  }
+  file_chooser.click();
 }
 
 var trigger_empty_warning = function () {
